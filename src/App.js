36d01842de640959b5c8332486287069b50a9dff
HusmithaSilva <husmitha.silva0@gmail.com>
2023-05-04 12:12:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import rocketImg from './assets/rocket.png';
+import { Signup } from "./components/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="">
+        <Signup />
+      </div>
+      <div className="">
+        {/* <img className="img-fluid w-100" src={rocketImg} alt=""/> */}
+      </div>
     </div>
   );
 }
